a12ac18cb2a62b81d69adddcf33cd1a3ec49b006
Colin Campbell <colinc86@gmail.com>
2019-10-05 19:47:17 -0500

    Move kubectl fetch to entrypoint.sh.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "${INPUT_GKEAPPLICATIONCREDENTIALS}" | base64 -d > /tmp/account.json
+gcloud auth configure-docker
+gcloud auth activate-service-account --key-file=/tmp/account.json
+gcloud config set project $INPUT_GKEPROJECTID
+gcloud container clusters get-credentials $INPUT_GKECLUSTERNAME --zone $INPUT_GKELOCATIONZONE --project $INPUT_GKEPROJECTID
+export KUBECONFIG=/.kube/config
+
 echo "Getting the status of ${INPUT_PODNAME}"
 kubectl get pods -n platform
 RESPONSE=$(echo kubectl get pods -n $INPUT_GKENAMESPACE)
